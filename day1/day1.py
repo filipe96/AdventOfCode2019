f664869ef5d7831b4ca8d3f78bef14f62f778c59
Philipp Seitz <p.seitz@netz98.de>
2019-12-01 17:55:52 +0100

    solves second part

--- day1/day1.py
+++ day1/day1.py
@@ -8,12 +8,18 @@ def getData():
 def calc(mass):
     return math.floor(mass / 3)  - 2
 
+def recurisiveFuleCalc(fuel):
+    if calc(fuel) <= 0:
+        return 0 
+    else: 
+        return calc(fuel) + recurisiveFuleCalc(calc(fuel))
+
 
 def main():
     sum = 0
     for mass in getData():
-        sum += calc(int(mass))
-    
+        sum += recurisiveFuleCalc(int(mass))
     print(sum)
+    print(recurisiveFuleCalc(sum))
 
 main()
