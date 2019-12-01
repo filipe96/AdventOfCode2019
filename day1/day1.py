93aeeb92b0fdc25b35c6c288cf75f6f5f65d91d8
Philipp Seitz <p.seitz@netz98.de>
2019-12-01 17:34:01 +0100

    solves first part

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,19 @@
-with open("input1.txt") as f:
-        data = f.read().split("\n")
+import math
 
-print(data)
+def getData():
+    with open("input1.txt") as f:
+            data = f.read().split("\n")
+    return data
+
+def calc(mass):
+    return math.floor(mass / 3)  - 2
+
+
+def main():
+    sum = 0
+    for mass in getData():
+        sum += calc(int(mass))
+    
+    print(sum)
+
+main()
